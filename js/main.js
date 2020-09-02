b314dc121895df4eaae9496c000480312115f9ef
Nedeljko Kostic <nedeljkokostic0@gmail.com>
2020-09-02 22:10:04 +0200

    JS anonymous functions #LAGI - 24

--- js/main.js
+++ js/main.js
@@ -17,3 +17,33 @@ function myFunc(){
 console.log('Var declared inside of the function = ' + inside);// Reference error - inside is not defined
 
 myFunc();
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const mapSquaredNumbers = numbers.map(x => x * x);
+
+console.log('Array numbers: ' + numbers);
+console.log('Array mapSquaredNumbers: ' + mapSquaredNumbers);
+
+const anonymousSquaredNumbers = function(){
+    let temp = [];
+    numbers.forEach(x => temp.push(x * x));
+    return temp;
+};
+
+console.log('Array numbers: ' + numbers);
+console.log('Array anonymousSquaredNumbers: ' + anonymousSquaredNumbers());
+
+(sum = function(x = 0, y = 0){ 
+    console.log('Sum of ' + x  + ' and ' + y + ' is: ' + (x + y));
+})();
+
+let numX = 10;
+let numY = 20;
+
+sum(numX, numY);
+
+let double = number => { return number * 2; };
+
+let doubleNumber = 10;
+console.log(doubleNumber + ' doubled: ' + double(doubleNumber));
